5d7ae338be3382138dcbf81784f99a3deac49db9
Heidi Mork <heidimork@yahoo.no>
2021-12-07 07:19:38 +0100

    Day 7

---
+++ src/main/kotlin/days/Day7.kt
@@ -0,0 +1,20 @@
+package days
+
+import functions.readLines
+import kotlin.math.absoluteValue
+
+private const val fileName = "day7.txt"
+private val input = readLines(fileName)
+private val testInput = """
+    16,1,2,0,4,2,7,1,2,14
+""".trimIndent().lines()
+
+fun main() {
+    val numbers = input.first().split(",").map { it.toInt() }
+    val min = numbers.minOf { it }
+    val max = numbers.maxOf { it }
+    val task1 = (min .. max).map{i -> numbers.map { n -> (n - i).absoluteValue }.sum()}.minOf { it }
+    println("Task1: $task1") //331067
+    val task2 = (min .. max).map{ i -> numbers.map { n -> (1.. (n - i).absoluteValue).sum() }.sum()}.minOf { it }
+    println("Task2: $task2") //92881128
+}
